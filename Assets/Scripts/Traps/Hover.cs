49dee412308edd73f20ace9395740e0a37613b37
Robert Ackley <ackleyrc@gmail.com>
2018-01-27 15:31:36 -0800

    Creting Neurotransmitters prototype

---
+++ Assets/Scripts/Traps/Hover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour {
+
+    float defaultY;
+    float offset;
+
+    public float amplitude;
+    public float rate;
+
+    private void Start()
+    {
+        defaultY = transform.position.y;
+        offset = Random.Range(0f, Mathf.PI);
+    }
+    
+    void Update () {
+        Vector3 pos = transform.position;
+        transform.position = new Vector3(pos.x, defaultY + amplitude * Mathf.Sin((Time.time + offset) * rate), pos.z);
+	}
+}
---
+++ Assets/Scripts/Traps/Neurotransmitters.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Neurotransmitters : MonoBehaviour {
+
+    float defaultX;
+    float defaultY;
+    float xOffset;
+    float yOffset;
+
+    public float xAmplitude;
+    public float yAmplitude;
+    public float xRate;
+    public float yRate;
+
+    private void Start()
+    {
+        defaultX = transform.position.x;
+        defaultY = transform.position.y;
+        xOffset = Random.Range(0f, Mathf.PI);
+        yOffset = Random.Range(0f, Mathf.PI);
+    }
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        float xPos = defaultY + xAmplitude * Mathf.Sin((Time.time + xOffset) * xRate);
+        float yPos = defaultY + yAmplitude * Mathf.Sin((Time.time + yOffset) * yRate);
+        transform.position = new Vector3(xPos, yPos, pos.z);
+    }
+}
