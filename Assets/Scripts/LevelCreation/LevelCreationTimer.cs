2a0620994c21735f51b2db1c72fe7a5a881649c6
Andrew Hung <druphoria@utexas.edu>
2018-01-27 01:17:48 -0800

    timer

---
+++ Assets/Scripts/LevelCreation/LevelCreationTimer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelCreationTimer : MonoBehaviour
+{
+    public float allowedLevelSetupTime;
+    Text timerText;
+
+	void Awake ()
+    {
+        timerText = GetComponent<Text>();
+        StartCoroutine(StartCountdown());
+	}
+	
+	IEnumerator StartCountdown()
+    {
+        float remainingLevelSetupTime = allowedLevelSetupTime + .5f; // to account for start delays
+        while (remainingLevelSetupTime > 0)
+        {
+            remainingLevelSetupTime -= Time.deltaTime;
+            timerText.text = "" + Mathf.Clamp(Mathf.Ceil(remainingLevelSetupTime), 1, allowedLevelSetupTime);
+            yield return null;
+        }
+
+        timerText.text = "GO";
+        Camera.main.GetComponent<CameraBehavior>().LerpToMatchStart();
+    }
+}
