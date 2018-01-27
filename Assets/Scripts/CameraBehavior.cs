9ee81106d0b0eafb2209fdcb7d4cac2fe074b3a1
Andrew Hung <druphoria@utexas.edu>
2018-01-27 00:54:32 -0800

    transition from level-editing view to scene view

---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour {
+
+    public float matchStartMovementSpeed;
+    public float matchStartRotationSpeed;
+    public Vector3 overheadPosition;
+    public Vector3 matchStartPosition;
+    public Vector3 matchStartRotation;
+
+	void Start ()
+    {
+        transform.position = overheadPosition;
+    }
+
+    public void LerpToMatchStart()
+    {
+        StartCoroutine(LerpToMatchStartPos());
+        StartCoroutine(LerpToMatchStartRot());
+    }
+
+    IEnumerator LerpToMatchStartPos()
+    {
+        float t = 0;
+        Vector3 startPos = transform.position;
+
+        while (t < 1)
+        {
+            t += Mathf.Clamp01(Time.deltaTime * matchStartMovementSpeed);
+            transform.position = Vector3.Lerp(startPos, matchStartPosition, t);
+
+            yield return null;
+        }
+    }
+
+    IEnumerator LerpToMatchStartRot()
+    {
+        float t = 0;
+        Quaternion startRot = transform.rotation;
+        Quaternion targetRot = Quaternion.Euler(matchStartRotation);
+
+        while (t < 1)
+        {
+            t += Mathf.Clamp01(Time.deltaTime * matchStartRotationSpeed);
+            transform.rotation = Quaternion.Slerp(startRot, targetRot, t);
+            yield return null;
+        }
+    }
+}
--- Assets/Scripts/ConnectionUI.cs
+++ Assets/Scripts/ConnectionUI.cs
@@ -3,11 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ConnectionUI : MonoBehaviour
     , IPointerEnterHandler
     , IPointerExitHandler
 {
+    float fadeRate = 1.25f;
+    Image img;
+
+    void Awake()
+    {
+        img = GetComponent<Image>();
+    }
+
     public void OnPointerEnter (PointerEventData eventData)
     {
         Debug.Log("entered");
@@ -18,4 +27,28 @@ public class ConnectionUI : MonoBehaviour
         Debug.Log("exited");
     }
 
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Camera.main.GetComponent<CameraBehavior>().LerpToMatchStart();
+            StartCoroutine(FadeOut());
+        }
+    }
+
+    IEnumerator FadeOut()
+    {
+        float t = 0;
+        while (t < 1)
+        {
+            t += Time.deltaTime * fadeRate;
+            Color col = img.color;
+            col.a = 1f - t;
+            img.color = col;
+
+            yield return null;
+        }
+
+        Destroy(gameObject);
+    }
 }
