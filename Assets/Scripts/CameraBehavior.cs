bfe5eaeb2c52fe011863e08f934de2ab2e489239
Andrew Hung <druphoria@utexas.edu>
2018-01-27 10:58:06 -0800

    transitions and stuff

--- Assets/Scripts/CameraBehavior.cs
+++ Assets/Scripts/CameraBehavior.cs
@@ -10,6 +10,13 @@ public class CameraBehavior : MonoBehaviour {
     public Vector3 raceStartPosition;
     public Vector3 raceStartRotation;
 
+    public float dogTransitionMovementSpeed;
+    public float dogTransitionRotationSpeed;
+    public Vector3 dogViewPosition;
+    public Vector3 dogViewRotation;
+
+    public float skyboxLerpRate;
+
 	void Start ()
     {
         transform.position = levelCreationCamPosition;
@@ -17,34 +24,56 @@ public class CameraBehavior : MonoBehaviour {
 
     public void LerpToRaceStart()
     {
-        StartCoroutine(LerpToRaceStartPos());
-        StartCoroutine(LerpToRaceStartRot());
+        StartCoroutine(LerpToPos(raceStartPosition, raceStartMovementSpeed));
+        StartCoroutine(LerpToRot(raceStartRotation, raceStartRotationSpeed));
     }
 
-    IEnumerator LerpToRaceStartPos()
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(LerpToPos(dogViewPosition, dogTransitionMovementSpeed));
+            StartCoroutine(LerpToRot(dogViewRotation, dogTransitionRotationSpeed));
+            StartCoroutine(LerpSky());
+        }
+    }
+
+    IEnumerator LerpToPos(Vector3 targetPos, float speed)
     {
         float t = 0;
         Vector3 startPos = transform.position;
 
         while (t < 1)
         {
-            t += Mathf.Clamp01(Time.deltaTime * raceStartMovementSpeed);
-            transform.position = Vector3.Lerp(startPos, raceStartPosition, t);
+            t += Mathf.Clamp01(Time.deltaTime * speed);
+            transform.position = Vector3.Lerp(startPos, targetPos, t);
 
             yield return null;
         }
     }
 
-    IEnumerator LerpToRaceStartRot()
+    IEnumerator LerpToRot(Vector3 targetRotation, float speed)
     {
         float t = 0;
         Quaternion startRot = transform.rotation;
-        Quaternion targetRot = Quaternion.Euler(raceStartRotation);
+        Quaternion targetRot = Quaternion.Euler(targetRotation);
 
         while (t < 1)
         {
-            t += Mathf.Clamp01(Time.deltaTime * raceStartRotationSpeed);
+            t += Mathf.Clamp01(Time.deltaTime * speed);
             transform.rotation = Quaternion.Slerp(startRot, targetRot, t);
+            yield return null;
+        }
+    }
+
+    IEnumerator LerpSky()
+    {
+        float t = 0;
+
+        while (t < 1)
+        {
+            t += Mathf.Clamp01(Time.deltaTime * skyboxLerpRate);
+            RenderSettings.skybox.SetFloat("_Blend", t);
             yield return null;
         }
     }
@@ -53,5 +82,10 @@ public class CameraBehavior : MonoBehaviour {
     public void HandleRoundStart(RoundStartEvent e)
     {
         LerpToRaceStart();
+    }
+
+    void OnDestroy()
+    {
+        RenderSettings.skybox.SetFloat("_Blend", 0);
     }
 }
