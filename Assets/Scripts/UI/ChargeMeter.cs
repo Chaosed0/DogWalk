a7830c9b074b973ba6b630672fe3e2c46139005e
chaosed0 <chaosed0@gmail.com>
2018-01-27 17:53:20 -0800

    New charge bar

--- Assets/Scripts/UI/ChargeMeter.cs
+++ Assets/Scripts/UI/ChargeMeter.cs
@@ -8,6 +8,10 @@ class ChargeMeter: MonoBehaviour
     public PlayerInput playerInput;
     public PlayerMovement playerMovement;
 
+    public Image sliderFill;
+    public Color initialColor;
+    public Color finalColor;
+
     Slider slider;
     CanvasGroup canvasGroup;
 
@@ -19,6 +23,19 @@ class ChargeMeter: MonoBehaviour
 
         playerInput.gameObject.Subscribe<PlayerInput.StartedChargingEvent>((x) => canvasGroup.alpha = 1.0f);
         playerInput.gameObject.Subscribe<PlayerInput.StoppedChargingEvent>((x) => canvasGroup.alpha = 0.0f);
-        playerInput.gameObject.Subscribe<PlayerInput.ChargeChangedEvent>((x) => slider.value = x.charge);
+        playerInput.gameObject.Subscribe<PlayerInput.ChargeChangedEvent>(OnChargeChanged);
+    }
+
+    void OnChargeChanged(PlayerInput.ChargeChangedEvent e)
+    {
+        slider.value = e.charge;
+        sliderFill.color = Color.Lerp(initialColor, finalColor, e.charge);
+
+        if (e.charge >= playerMovement.burstChargeMin && e.charge <= playerMovement.burstChargeMax)
+        {
+            sliderFill.color = new Color(Random.Range(initialColor.r, finalColor.r),
+                                        Random.Range(initialColor.g, finalColor.g),
+                                        Random.Range(initialColor.b, finalColor.b));
+        }
     }
 }
