dc01263f24a5b4ffd7dd410afbdc5ecfd60ff21b
Andrew W Hung <druphoria@utexas.edu>
2018-01-28 04:59:08 -0800

    testing some stuff out

--- Assets/Scripts/UI/UVScroll.cs
+++ Assets/Scripts/UI/UVScroll.cs
@@ -6,10 +6,12 @@ public class UVScroll : MonoBehaviour {
 
     public Material mat;
     public Vector2 offset = new Vector2();
-    public float rate = 3f;
+    public Vector2 rate;
 
 	void Update () {
-        offset.x += rate * Time.deltaTime;
+        offset.x += rate.x * Time.deltaTime;
+        offset.y += rate.y * Time.deltaTime;
+
         mat.SetTextureOffset("_MainTex", offset);
 	}
 }
