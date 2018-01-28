537ab27fc53e9b6ac192b02fc0ad754f8e0df168
Robert Ackley <ackleyrc@gmail.com>
2018-01-28 15:53:18 -0800

    Adding in music and City/Birds ambient sounds

---
+++ Assets/Scripts/AmbientSound.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientSound : MonoBehaviour {
+
+    public AudioSource citySource;
+    public AudioSource birdsSource;
+
+    [SubscribeGlobal]
+    public void HandleDogSequenceStart(StartDogSequenceEvent e)
+    {
+        if (citySource != null && citySource.clip != null)
+        {
+            citySource.Play();
+        }
+
+        if (birdsSource != null && birdsSource.clip != null)
+        {
+            birdsSource.Play();
+        }
+    }
+
+    [SubscribeGlobal]
+    public void HandleDogSequenceStop(EndDogSequenceEvent e)
+    {
+        if (citySource != null && citySource.clip != null)
+        {
+            citySource.Stop();
+        }
+
+        if (birdsSource != null && birdsSource.clip != null)
+        {
+            birdsSource.Stop();
+        }
+    }
+}
---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour {
+
+    public AudioSource downTempoMusic;
+    public AudioSource upTempoMusic;
+
+    private void Awake()
+    {
+        if (downTempoMusic != null && downTempoMusic.clip != null)
+        {
+            downTempoMusic.Play();
+        }
+    }
+
+    [SubscribeGlobal]
+    public void HandleRoundStart(RoundStartEvent e)
+    {
+        if (downTempoMusic != null && downTempoMusic.clip != null)
+        {
+            // Debug.Log("Stopping Downtempo");
+            downTempoMusic.Stop();
+        }
+
+        if (upTempoMusic != null && upTempoMusic.clip != null)
+        {
+            if (!upTempoMusic.isPlaying)
+            {
+                // Debug.Log("Starting Uptempo");
+                upTempoMusic.Play();
+            }
+        }
+    }
+
+    [SubscribeGlobal]
+    public void HandleLevelCreationStart(LevelCreationStartEvent e)
+    {
+        if (upTempoMusic != null && upTempoMusic.clip != null)
+        {
+            // Debug.Log("Stopping Uptempo");
+            upTempoMusic.Stop();
+        }
+
+        if (downTempoMusic != null && downTempoMusic.clip != null)
+        {
+            if (!downTempoMusic.isPlaying)
+            {
+                // Debug.Log("Starting Downtempo");
+                downTempoMusic.Play();
+            }
+        }
+    }
+
+    [SubscribeGlobal]
+    public void HandleDogSequenceStart(StartDogSequenceEvent e)
+    {
+        if (upTempoMusic != null && upTempoMusic.clip != null)
+        {
+            upTempoMusic.Stop();
+        }
+
+        if (downTempoMusic != null && downTempoMusic.clip != null)
+        {
+            upTempoMusic.Stop();
+        }
+    }
+}
