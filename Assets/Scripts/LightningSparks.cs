a77055bf250b0fc11e389f4dc78c41051a464431
Andrew W Hung <druphoria@utexas.edu>
2018-01-28 03:19:57 -0800

    add lightning to axons

---
+++ Assets/Scripts/LightningSparks.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningSparks : MonoBehaviour {
+
+	void Awake () {
+        ParticleSystem ps = GetComponent<ParticleSystem>();
+        ParticleSystem.ShapeModule shape = ps.shape;
+        shape.mesh = GetComponent<MeshFilter>().mesh;
+	}
+}
