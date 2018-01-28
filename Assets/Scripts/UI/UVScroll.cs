cc11c86b41bebcbddd950ae1456db959d32f4936
Andrew Hung <druphoria@utexas.edu>
2018-01-27 21:21:08 -0800

    UIBeam effect

---
+++ Assets/Scripts/UI/UVScroll.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UVScroll : MonoBehaviour {
+
+    public Material mat;
+    public Vector2 offset = new Vector2();
+    public float rate = 3f;
+
+	void Update () {
+        offset.x += rate * Time.deltaTime;
+        mat.SetTextureOffset("_MainTex", offset);
+	}
+}
