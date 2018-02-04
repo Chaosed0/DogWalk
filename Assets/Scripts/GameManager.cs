55eba929be881839cf782d91f80cadeb22ccde76
chaosed0 <chaosed0@gmail.com>
2018-02-04 12:56:49 -0800

    Only give points when the runner actually finishes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -136,7 +136,11 @@ public class GameManager : MonoBehaviour {
         graph.finishNode = null;
 
         // ghetto score heuristic - tweak later
-        float scoreBonus = Mathf.Max(.2f, e.remainingTime / timer.raceTime);
+        float scoreBonus = 0f;
+        if (e.remainingTime > 0.0f)
+        {
+            scoreBonus = .2f + e.remainingTime / timer.raceTime;
+        }
         playerOneScore += currentPlayer == 1 ? scoreBonus : 0;
         playerTwoScore += currentPlayer == 2 ? scoreBonus : 0;
 
