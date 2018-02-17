e2eef6602397c3ed07b1672c6d5eea7e879c8887
Robert Ackley <ackleyrc@gmail.com>
2018-02-17 15:50:23 -0800

    Highlight current player between sequences

--- Assets/Scripts/UI/CurrentPlayerIndicator.cs
+++ Assets/Scripts/UI/CurrentPlayerIndicator.cs
@@ -5,16 +5,49 @@ using UnityEngine.UI;
 
 public class CurrentPlayerIndicator : MonoBehaviour {
 
+    public float scaleSpeeed = 10f;
+
+    public Color[] defaultColors;
+    public Color[] highlightColors;
+
+    private int fontSizeMin = 80;
+    private int fontSizeMax = 160;
+
+    [Range(80, 160)]
+    private float currentFontSize;
+
+    private int currentPlayer = 0;
+
     Text text;
 
     void Awake()
     {
         text = GetComponent<Text>();
+        HighlightText();
     }
 
-	[SubscribeGlobal]
+    private void Update()
+    {
+        if (currentFontSize > fontSizeMin)
+        {
+            currentFontSize = currentFontSize - Mathf.Min(scaleSpeeed * Time.deltaTime, currentFontSize - fontSizeMin);
+            text.fontSize = (int) currentFontSize;
+            text.color = Color.Lerp(defaultColors[currentPlayer], highlightColors[currentPlayer], (currentFontSize - fontSizeMin) / (fontSizeMax - fontSizeMin));
+        }
+    }
+
+    [SubscribeGlobal]
     public void HandleTogglePlayerEvent(ToggleCurrentPlayerEvent e)
     {
-        text.text = "Current Player: " + e.currentPlayer;
+        text.text = "PLAYER " + e.currentPlayer;
+        currentPlayer = e.currentPlayer - 1;
+        HighlightText();
+    }
+
+    private void HighlightText()
+    {
+        text.fontSize = fontSizeMax;
+        currentFontSize = fontSizeMax;
+        text.color = highlightColors[currentPlayer];
     }
 }
